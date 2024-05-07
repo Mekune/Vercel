e6162f931eca64f663de5e89ee2f0919ba2f8a4a
Mekune <nans.diaz30@gmail.com>
2024-05-07 15:17:51 +0200

    v13 get test

--- api/index.js
+++ api/index.js
@@ -41,7 +41,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", (req, res) => {
+app.get("/", (req, res) => {
   res.send("Hello World");
 });
 app.use("/users", usersRouter);
