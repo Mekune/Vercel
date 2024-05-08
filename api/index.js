78b3ad8d520dec863f87a09a7f597f8b45a3e3e9
Mekune <nans.diaz30@gmail.com>
2024-05-08 15:41:01 +0200

    alors peut être

--- api/index.js
+++ api/index.js
@@ -20,7 +20,7 @@ var maoRouter = require("./routes/mao");
 
 var app = express();
 // app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-app.use(cors({ origin: "http://localhost:3000" }));
+app.use(cors({ origin: process.env.REACT_APP_FRONT_URL }));
 
 // view engine setup
 // app.set("views", path.join(__dirname, "views"));
