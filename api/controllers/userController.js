016017eb016bd9d954d21de4fcb2d08298efa9a1
Mekune <nans.diaz30@gmail.com>
2024-05-08 18:10:00 +0200

    error 500 2

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -54,7 +54,6 @@ exports.getIdByUsername = async (req, res, next) => {
     return next(error); // Passer l'erreur au middleware de gestion des erreurs
   }
 };
-
 exports.connexion = async (req, res) => {
   const { username, password } = req.body;
   try {
@@ -76,13 +75,14 @@ exports.connexion = async (req, res) => {
 
     res.json({ token });
   } catch (error) {
-    console.error("Login error:", error);
+    console.error("Login error:", error); // Ajout de cette ligne pour voir l'erreur dans la console
     res.status(500).json({ message: "Erreur serveur" });
   }
 };
+
 exports.updateUserById = async (req, res) => {
   const { id } = req.params; // Récupérer l'ID de l'utilisateur depuis les paramètres de la requête
-  const { acutalUsername, username, password } = req.body;
+  const { actualUsername, username, password } = req.body;
 
   try {
     // Vérifiez si l'ID fourni est un ID MongoDB valide
@@ -105,20 +105,26 @@ exports.updateUserById = async (req, res) => {
 
     hashedPassword = bcrypt.hashSync(password, salt);
 
+    // Obtenir l'utilisateur actuel
+    const user = await User.findById(id);
+
+    if (!user) {
+      return res.status(404).json({ message: "Utilisateur non trouvé" });
+    }
+
     // Obtenir la liste des usernames existants
     let existingUsernames = await exports.getAllUsername();
 
-    // Récupérer l'username actuel
-
     // Exclure l'username actuel de la liste existingUsernames
     existingUsernames = existingUsernames.filter(
-      (username) => username !== acutalUsername
+      (username) => username !== actualUsername
     );
 
     // Vérifier si l'username existe déjà dans la liste
     if (username && existingUsernames.includes(username)) {
       return res.status(400).json({ message: "Cet username existe déjà" });
     }
+
     // Préparer les données à mettre à jour
     const updatedUserData = {};
     if (username) {
@@ -130,10 +136,6 @@ exports.updateUserById = async (req, res) => {
     const updatedUser = await User.findByIdAndUpdate(id, updatedUserData, {
       new: true,
     });
-
-    if (!updatedUser) {
-      return res.status(404).json({ message: "Utilisateur non trouvé" });
-    }
 
     res.status(200).json(updatedUser);
   } catch (error) {
